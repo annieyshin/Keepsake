f78f674f7778bed24203bdc9eb86d7f254d9d458
Annie Shin <annieyshin@gmail.com>
2018-10-04 08:24:02 -0700

    add pivotal scripts gem

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'devise'
+gem 'pivotal_git_scripts'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :views
   devise_for :users
-  root 'items#index'
+  root 'pages#index'
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
